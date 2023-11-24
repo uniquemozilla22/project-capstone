077f179e4513abb2dfa66f1e99f342335f5e4bd5
uniquemozilla22 <uniqgaming26@gmail.com>
2023-11-24 12:30:34 -0500

    Tailwind Configuration

--- src/App.tsx
+++ src/App.tsx
@@ -4,23 +4,11 @@ import "./App.css";
 function App() {
   const [count, setCount] = useState(0);
 
-  useEffect(async () => {
-    const data = await fetch("http://localhost:3000/users");
-
-    const users = await data.json();
-
-    const cartData = users[0].cart.map(async (product, index) => {
-      const responseCart = await fetch(
-        "http://localhost:3000/products/" + product
-      );
-
-      const cartData = await responseCart.json();
-      users[0].cart[index] = cartData;
-    });
-    console.log(users, "this is the updated users data with cart data in it");
-  }, []);
-
-  return <></>;
+  return (
+    <>
+      <h1>Hello </h1>
+    </>
+  );
 }
 
 export default App;
